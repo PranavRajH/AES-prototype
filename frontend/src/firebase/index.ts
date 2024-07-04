e15080c597d1a542ae71810ae1400ae920d4ddb3
Pranav Raj H <vinstarpranav@gmail.com>
2024-07-04 11:42:14 +0530

    Update index.ts for security reason

--- frontend/src/firebase/index.ts
+++ frontend/src/firebase/index.ts
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC36BoN2-17su2P9HJYtUhdp-URq5IV5dA",
-  authDomain: "hacksavvy-aes-project.firebaseapp.com",
-  projectId: "hacksavvy-aes-project",
-  storageBucket: "hacksavvy-aes-project.appspot.com",
-  messagingSenderId: "983549091463",
-  appId: "1:983549091463:web:a7748ad72a7e87409ca222",
-  measurementId: "G-Z1HSK0YYZ4",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
